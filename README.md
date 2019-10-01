bdcb044c1ec2cee669b18d133f32938b46e07e1a
David Gutsch <davidgutsch@Davids-MBP-2.attlocal.net>
2019-10-01 06:27:47 -0400

    updating gitignore after weekend of tutorials

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # React-Native-Udemy
+
+##initializes an application
+npx expo-cli init <app name>
+##installs gestures, reanimation, and navigation
+npx expo-cli install react-native-gesture-handler react-native-reanimated react-navigation-stack
---
+++ blog-app/src/screens/IndexScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet}  from 'react-native';
+
+const indexscreen = () => {
+
+    return (
+        <View>
+            <Text>index screen</Text>
+        </View>
+    )
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default indexscreen;
+
